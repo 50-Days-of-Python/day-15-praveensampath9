83a54665767e0d453cee9c1e61d855bf4f23399d
praveensampath9 <101631187+praveensampath9@users.noreply.github.com>
2023-01-28 09:46:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #Write your code here.
-import locale
-numbers = input().split(",")
-numbers = [str(num) for num in numbers]
-locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
-for i in range(len(numbers)):
-    numbers[i] = locale.format("%d", int(numbers[i]), grouping=True)
-print(numbers)
+num = eval(input())
+format_num = []
+for i in num:
+    x="{:,}".format(i) 
+    format_num.append(str(x))
+print(format_num)
